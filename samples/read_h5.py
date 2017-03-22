4480b83abe784f9a26458b3bdedadd74404f2206
Patrick Durand <patrick.durand@inria.fr>
2017-03-22 15:15:16 +0100

    add a sample pyGATB snippet

---
+++ samples/read_h5.py
@@ -0,0 +1,14 @@
+import os
+from pyGATB.graph import Graph
+
+F_NAME='celegans_reads.h5'
+
+print ("File size: %.d kb" % (os.stat(F_NAME).st_size / 1024))
+
+graph = Graph(F_NAME)
+
+graph
+
+for i, node in enumerate(graph):
+    print('{}: {!r}'.format(i, node))
+    if i > 10: break
