e728fd03ae79b012565fc79227117dae21eb788c
Patrick Durand <patrick.durand@inria.fr>
2017-03-22 17:06:46 +0100

    fix after tests on Linux/OSX

--- docker/py-gatb-compile.sh
+++ docker/py-gatb-compile.sh
@@ -36,27 +36,31 @@ if [ ! ${GIT_PROVIDER} == "ci" ]; then
   fi
 
   # Figure out whether or not we have to get GATB-Core source code
-  if [ ! -d ${PYGATB_SOURCE} ]; then
-    git clone --single-branch --depth 1 -b ${GIT_BRANCH} ${PYGATB_GITURL} ${PYGATB_SOURCE}
-    cd ${PYGATB_SOURCE} && git submodule init && git submodule update --depth 1
+  cd ${PYGATB_SOURCE}
+  if [ ! -d "pyGATB" ]; then
+    git clone --single-branch --depth 1 -b ${GIT_BRANCH} ${PYGATB_GITURL}
+    cd pyGATB && git submodule init && git submodule update --depth 1
   else
-    cd ${PYGATB_SOURCE}
+    cd pyGATB
     git checkout ${GIT_BRANCH}
   fi
 fi
 
 # Prepare a fresh build directory
-if [ -d ${PYGATB_BUILD} ]; then
-    rm -rf ${PYGATB_BUILD}
-fi
-mkdir -p ${PYGATB_BUILD}
 cd ${PYGATB_BUILD}
+if [ -d "build" ]; then
+    rm -rf build
+fi
+
+mkdir -p build
+cd build
 
 # Compile source code
-cmake -D CMAKE_BUILD_TYPE=Release ${PYGATB_SOURCE} $@
+cmake -D CMAKE_BUILD_TYPE=Release ${PYGATB_SOURCE}/pyGATB $@
 make ${PARALLEL_OPT}
 
 # Test, distribute and install
 python3 setup.py test
-python3 setup.py bdist
+python3 setup.py bdist_egg
 python3 setup.py install
+
