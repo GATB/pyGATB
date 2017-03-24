11a345dd45b93ba364615af10753ed0b185490cc
Patrick Durand <patrick.durand@inria.fr>
2017-03-24 15:16:16 +0100

    update snippet

--- samples/read_h5.py
+++ samples/read_h5.py
@@ -1,12 +1,15 @@
-import os
+# we import pyGATB Graph
 from pyGATB.graph import Graph
 
+# We will use a file containing a De Bruijn Graph stored
+# in HDF5 format; file created using dbgh5 tool provided
+# with GATB-Core. (This file is located next to this snippet)
 F_NAME='celegans_reads.h5'
 
-print ("File size: %.d kb" % (os.stat(F_NAME).st_size / 1024))
-
+# We create the graph
 graph = Graph('-in %s' % F_NAME)
 
+# We iterate over some nodes
 for i, node in enumerate(graph):
     print('{}: {!r}'.format(i, node))
     if i > 10: break
